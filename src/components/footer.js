dbd156d048e6c5862a49f0853d84b7ed9bbabc57
Carlos Martinez <martinezcj2@gmail.com>
2016-10-17 11:57:32 -0400

    Added footer

--- src/components/footer.js
+++ src/components/footer.js
@@ -4,7 +4,25 @@ const Footer = React.createClass({
   render: function(){
     return (
       <footer className="footerSection">
+        <div className="footerInfo">
+          <h1>ITTA</h1>
+          <p>Pop-up banh mi offal normcore.  Copper mug pok pok meggings hoodie lo-fi polaroid flexitarian, fap synth.  Cardigan whatever bushwick, microdosing XOXO tilde poke PBR</p>
+          <div>
+            <i className="facebook-square"></i>
+          </div>
+        </div>
+        <div className="footerLinks">
+          <ul>
+            <li>About</li>
+            <li>Map</li>
+            <li>Download</li>
+            <li>Our Blog</li>
+            <li>Careers</li>
+          </ul>
+        </div>
+        {/* <div  className="footerBottom">
 
+        </div> */}
       </footer>
     )
   }
